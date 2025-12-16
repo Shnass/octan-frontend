0ba11803af5cf01ac81b7bf5fb1c9969f3ecafef
alexandr Kudryavtsev <3030.doc@gmail.com>
2025-12-16 12:48:44 +0200

    db test + vercel test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,20 @@
-export default function About() {
+import pool from "@/lib/db";
+export default async function About() {
+    const { rows } = await pool.query("SELECT * FROM records LIMIT 20");
     return (
         <>
             <h2>About Us</h2>
+
+            <div>
+            {rows.map((r) => (
+                <div key={r.id}>
+                <strong>{r.artist}</strong> – {r.name} ({r.year})
+                </div>
+            ))}
+            </div>
         </>
     )
 }
+
+
+
